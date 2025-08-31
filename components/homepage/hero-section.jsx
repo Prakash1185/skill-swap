ae1f44719f041a24a733da0811008ee920332e17
Prakash Kumar <pgcet601@gmail.com>
2025-08-31 15:24:34 +0530

    project started

---
+++ components/homepage/hero-section.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from '../ui/button'
+
+const HeroSection = () => {
+  return (
+    <div>
+
+    <Button>This is a button</Button>
+
+    </div>
+  )
+}
+
+export default HeroSection
---
+++ components/homepage/homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroSection from './hero-section'
+
+const Homepage = () => {
+  return (
+    <>
+  <HeroSection/>
+    </>
+  )
+}
+
+export default Homepage
